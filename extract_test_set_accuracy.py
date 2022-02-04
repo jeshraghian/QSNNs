3d92f32ac305a1724c0cb3342a1b58537b52e25c
Corey Lammie <coreylammie@gmail.com>
2022-02-04 15:48:12 +1000

    Formatted files using black

--- extract_test_set_accuracy.py
+++ extract_test_set_accuracy.py
@@ -30,17 +30,35 @@ data = {
 }
 
 
-
-df = pd.DataFrame(columns=["dataset", "network_precision", "scheduler", "test_set_accuracy_best", "test_set_accuracy_mean", "test_set_accuracy_std"])
+df = pd.DataFrame(
+    columns=[
+        "dataset",
+        "network_precision",
+        "scheduler",
+        "test_set_accuracy_best",
+        "test_set_accuracy_mean",
+        "test_set_accuracy_std",
+    ]
+)
 for precision in data.keys():
     for dataset_idx, dataset in enumerate(data[precision].keys()):
         for scheduler in data[precision][dataset].keys():
             test_set_accuracy_values = []
             for idx, trial in enumerate(data[precision][dataset][scheduler]):
                 trial_df = pd.read_csv(trial)
-                test_set_accuracy = trial_df['test_acc'].max().item()
+                test_set_accuracy = trial_df["test_acc"].max().item()
                 test_set_accuracy_values.append(test_set_accuracy)
 
-            df = df.append({'dataset': dataset, 'network_precision': precision, 'scheduler': scheduler, 'test_set_accuracy_best': max(test_set_accuracy_values), 'test_set_accuracy_mean': np.mean(test_set_accuracy_values), 'test_set_accuracy_std': np.std(test_set_accuracy_values)}, ignore_index=True)
+            df = df.append(
+                {
+                    "dataset": dataset,
+                    "network_precision": precision,
+                    "scheduler": scheduler,
+                    "test_set_accuracy_best": max(test_set_accuracy_values),
+                    "test_set_accuracy_mean": np.mean(test_set_accuracy_values),
+                    "test_set_accuracy_std": np.std(test_set_accuracy_values),
+                },
+                ignore_index=True,
+            )
 
-df.to_csv('test_set_accuracy.csv', index=False)
+df.to_csv("test_set_accuracy.csv", index=False)
